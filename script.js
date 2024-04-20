b00e9832c97171c89f1fda9ef363bdfc302b9fe5
CatSema <catsemaofficial@gmail.com>
2024-04-20 13:54:30 +0300

    new file:   clicks/style.css 	new file:   clicks/index.html 	new file:   img/background/deversee_poster_big.webp 	new file:   img/background/education-level-poster-big.webp 	new file:   img/background/haken_poster_big.webp 	new file:   img/background/ivan_durnev_gameverse_poster_big.webp 	new file:   img/background/parking_jam_poster_big.webp 	new file:   img/background/polygami_poster_big.webp 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+     card.addEventListener('click', () => {
+          // Обработчик клика по карточке
+          console.log('Карточка нажата!');
+          // Вы можете добавить сюда нужный функционал, например, переход на другую страницу
+     });
+});
