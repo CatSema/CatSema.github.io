144134cdaac56fb7c2309175a027cc655d060e73
CatSema <catsemaofficial@gmail.com>
2024-04-21 01:19:00 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let tg = window.Telegram.WebApp;
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+     card.addEventListener('click', () => {
+          // Обработчик клика по карточке
+          console.log('Карточка нажата!');
+          // Вы можете добавить сюда нужный функционал, например, переход на другую страницу
+     });
+});
