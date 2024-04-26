4041d03a2b723ed91a244aeeb8e75d7f16b664b9
CatSema <catsemaofficial@gmail.com>
2024-04-27 01:38:19 +0300

    modified:   airdrop/index.html 	modified:   airdrop/style.css 	modified:   clicks/index.html 	modified:   clicks/style.css 	new file:   favicon.ico 	modified:   games/index.html 	modified:   games/style.css 	new file:   img/icons/miners/BullApp.jpg 	new file:   img/icons/miners/MomoAI.jpg 	new file:   img/icons/miners/QappiMiner.jpg 	new file:   img/icons/miners/UniverseTgGame.jpg 	new file:   img/icons/miners/bnbcoldwallet.jpg 	new file:   img/icons/miners/brrrrrgame.jpg 	new file:   img/icons/miners/fueljetton.jpg 	new file:   img/icons/miners/herewallet.jpg 	new file:   img/icons/miners/nearharvestmoon.jpg 	new file:   img/icons/miners/pixel-wallet.jpg 	new file:   img/icons/miners/pocketfi.jpg 	new file:   img/icons/miners/vertus-app.jpg 	new file:   img/icons/nft/cube-worlds.jpg 	modified:   index.html 	new file:   miners/index.html 	new file:   miners/style.css 	new file:   nft/index.html 	new file:   nft/style.css 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card')
 
 cards.forEach(card => {
-     card.addEventListener('click', () => {
-          // Обработчик клика по карточке
-          console.log('Карточка нажата!');
-          // Вы можете добавить сюда нужный функционал, например, переход на другую страницу
-     });
-});
+	card.addEventListener('click', () => {
+		// Обработчик клика по карточке
+		console.log('Карточка нажата!')
+		// Вы можете добавить сюда нужный функционал, например, переход на другую страницу
+	})
+})
